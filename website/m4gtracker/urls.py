5a241f09e9ba3308fa16b7a64e7e2e37e6b43a7f
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@gmail.com>
2023-03-21 04:28:27 +0100

    Website update

--- website/m4gtracker/urls.py
+++ website/m4gtracker/urls.py
@@ -1,12 +1,37 @@
 #m4gtracker URL Configuration
 from django.contrib 	import admin
 from django.urls 	    import path, include
-from main.views         import home
+from main.views         import home, m0k1
 
 urlpatterns = [
+    # Landing
     path(''		            , home                                  ),
-    path('<slug:slug>/'     , home                                  ),
+        
+    # Framework
     path('accounts/'        , include('django.contrib.auth.urls')   ), # login, logout
     path('admin/'	        , admin.site.urls                       ),
+
+    # M0-K1
+    path('m0-k1/'           , m0k1                                  ),
+    path('mO-k1/'           , m0k1                                  ),
+    path('mo-k1/'           , m0k1                                  ),
+    path('m0k1/'            , m0k1                                  ),
+    path('mOk1/'            , m0k1                                  ),
+    path('mok1/'            , m0k1                                  ),
+    path('m0-ki/'           , m0k1                                  ),
+    path('mO-ki/'           , m0k1                                  ),
+    path('mo-ki/'           , m0k1                                  ),
+    path('m0ki/'            , m0k1                                  ),
+    path('mOki/'            , m0k1                                  ),
+    path('moki/'            , m0k1                                  ),
+    path('m0-kI/'           , m0k1                                  ),
+    path('mO-kI/'           , m0k1                                  ),
+    path('mo-kI/'           , m0k1                                  ),
+    path('m0kI/'            , m0k1                                  ),
+    path('mOkI/'            , m0k1                                  ),
+    path('mokI/'            , m0k1                                  ),
+    
+    # Fallback
+    path('<slug:slug>/'     , home                                  ),
 ]
  
--- website/main/views.py
+++ website/main/views.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect,HttpResponse
 from django.shortcuts import render
+from django.contrib.staticfiles.utils   import get_files
+from django.contrib.staticfiles.storage import StaticFilesStorage
+import os
+
+@staticmethod
+def getSnapshots():
+    res = []
+    for path in os.listdir('static/snapshots'):
+        if os.path.isfile(os.path.join('static/snapshots', path)):
+            res.append(path)
+    return res
 
 @login_required()
 def profile(request):
@@ -20,7 +31,13 @@ def home(request, slug=None):
             return HttpResponse(0)
     request.session.set_test_cookie()
     if slug=='profile':     return render(request, 'profile.html'       , context={ 'section' : 'User Profile'      } )
+    if slug=='snapshots':   return render(request, 'snapshots.html'     , context={ 'section' : 'Snapshot Gallery'  , 'data' : getSnapshots() } )
     if slug=='bugtracker':  return render(request, 'bugtracker.html'    , context={ 'section' : 'BUGTracker'        } )
-    if slug in('moki','mok1','m0ki','m0k1','mo-ki','mo-k1','m0-ki','m0-k1'): 
-                            return render(request, 'm0-k1.html', context={ 'section': 'M0-K1 Synthesizer'})
+    if slug in('moki','mok1','m0ki','m0k1','mo-ki','mo-k1','m0-ki','m0-k1'): return m0k1(request)
     return render(request, 'home.html')
+
+def m0k1(request):
+    section = request.GET.get('section', None)
+    if section == 'keys': return render(request, 'm0-k1-keys.html', context={'section': 'M0-K1 Keyboard Explanation' })
+    return render(request, 'm0-k1.html', context={ 'section': 'M0-K1 Synthesizer'})
+    
